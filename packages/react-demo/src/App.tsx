3fb02c42aab1c51dd78b4ea8eb90a2d42d01843e
Alexander <valverin95@gmail.com>
2023-09-15 11:25:29 +0300

    elevations

--- packages/react-demo/src/App.tsx
+++ packages/react-demo/src/App.tsx
@@ -1,9 +1,22 @@
 function App() {
   return (
     <div className="App">
-      <button className="btn btn-red">btn</button>
+      <button className="btn btn-elevated">Elevated button</button>
       <br />
-      <p className="test">test</p>
+      <p className="text-primary">test</p>
+
+      <section className="flex gap-2">
+        <div className="h-10 w-10 elevation-1"></div>
+        <div className="h-10 w-10 elevation-2"></div>
+        <div className="h-10 w-10 elevation-3"></div>
+        <div className="h-10 w-10 elevation-4"></div>
+        <div className="h-10 w-10 elevation-5"></div>
+        <div className="h-10 w-10 elevation-6"></div>
+        <div className="h-10 w-10 elevation-7"></div>
+        <div className="h-10 w-10 elevation-8"></div>
+        <div className="h-10 w-10 elevation-9"></div>
+        <div className="h-10 w-10 elevation-10"></div>
+      </section>
     </div>
   );
 }
--- packages/tailwind/index.ts
+++ packages/tailwind/index.ts
@@ -2,6 +2,21 @@ import plugin from 'tailwindcss/plugin';
 import { AddComponents } from './components';
 import tokens from './tokens.json';
 
+/** @See https://material-components.github.io/material-components-web-catalog/#/component/elevation */
+const elevation = {
+  0: '0 0 0 0 rgba(0,0,0,.2), 0 0 0 0 rgba(0,0,0,.14), 0 0 0 0 rgba(0,0,0,.12);',
+  1: '0 2px 1px -1px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 3px 0 rgba(0,0,0,.12);',
+  2: '0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12);',
+  3: '0 3px 3px -2px rgba(0,0,0,.2), 0 3px 4px 0 rgba(0,0,0,.14), 0 1px 8px 0 rgba(0,0,0,.12);',
+  4: '0 2px 4px -1px rgba(0,0,0,.2), 0 4px 5px 0 rgba(0,0,0,.14), 0 1px 10px 0 rgba(0,0,0,.12);',
+  5: '0 3px 5px -1px rgba(0,0,0,.2), 0 5px 8px 0 rgba(0,0,0,.14), 0 1px 14px 0 rgba(0,0,0,.12);',
+  6: '0 3px 5px -1px rgba(0,0,0,.2), 0 6px 10px 0 rgba(0,0,0,.14), 0 1px 18px 0 rgba(0,0,0,.12);',
+  7: '0 4px 5px -2px rgba(0,0,0,.2), 0 7px 10px 1px rgba(0,0,0,.14), 0 2px 16px 1px rgba(0,0,0,.12);',
+  8: '0 5px 5px -3px rgba(0,0,0,.2), 0 8px 10px 1px rgba(0,0,0,.14), 0 3px 14px 2px rgba(0,0,0,.12);',
+  9: '0 5px 6px -3px rgba(0,0,0,.2), 0 9px 12px 1px rgba(0,0,0,.14), 0 3px 16px 2px rgba(0,0,0,.12);',
+  10: '0 6px 6px -3px rgba(0,0,0,.2), 0 10px 14px 1px rgba(0,0,0,.14), 0 4px 18px 3px rgba(0,0,0,.12);'
+}
+
 const colors = tokens.entities
   .filter(entitiy => entitiy.type === 'color')
   .reduce((acc, token) => {
@@ -13,22 +28,22 @@ const colors = tokens.entities
     return acc;
   }, { 'onPrimary': 'pink' } as Record<string, string>)
 
-export default plugin(function ({ addComponents, theme }) {
-  console.log('colors', colors);
-
+export default plugin(function ({ addComponents, matchUtilities, theme }) {
   addComponents({
     ...AddComponents(theme)
   });
+
+  matchUtilities(
+    {
+      elevation: (value) => ({
+        boxShadow: value
+      })
+    },
+    { values: theme('elevation') }
+  )
 }, {
   theme: {
-    /** @See https://material-components.github.io/material-components-web-catalog/#/component/elevation */
-    elevation: {
-      0: '0 0 0 0 rgba(0,0,0,.2), 0 0 0 0 rgba(0,0,0,.14), 0 0 0 0 rgba(0,0,0,.12);',
-      1: '0 2px 1px -1px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 3px 0 rgba(0,0,0,.12);',
-      2: '0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12);',
-      3: '0 3px 3px -2px rgba(0,0,0,.2), 0 3px 4px 0 rgba(0,0,0,.14), 0 1px 8px 0 rgba(0,0,0,.12);',
-      4: '0 2px 4px -1px rgba(0,0,0,.2), 0 4px 5px 0 rgba(0,0,0,.14), 0 1px 10px 0 rgba(0,0,0,.12);',
-    },
+    elevation,
     extend: {
       colors,
       borderRadius: {
