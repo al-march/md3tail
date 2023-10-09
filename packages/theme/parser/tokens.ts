c5dbada5801b82d6ee31d25913bb77d9af6fbd11
Alexander <valverin95@gmail.com>
2023-10-09 16:04:49 +0300

    add tokens parser

--- packages/theme/parser/tokens.ts
+++ packages/theme/parser/tokens.ts
@@ -1,38 +1,397 @@
-import tokens from '../tokens.json';
-import { toCamelCase } from '../utils'
-const lightColors = tokens.entities
-  .filter(entitiy => entitiy.type === 'color')
-  .reduce((acc, token) => {
-    const name = token.name.split('.');
-    const isLight = name.at(-1) === 'light';
-    if (isLight) {
-      /**
-       * colorName - name of color without prefixes
-       * @Example md.sys.color.secondary-container.light => secondaryContainer
-       */
-      let colorName = name.slice(3, -1).join('');
-      acc[toCamelCase(colorName)] = token.value;
-    }
+export const cssTokens = `
+:root {
+  --md-source: #00677f;
+  /* primary */
+  --md-ref-palette-primary0: #000000;
+  --md-ref-palette-primary10: #001f28;
+  --md-ref-palette-primary20: #003543;
+  --md-ref-palette-primary25: #004152;
+  --md-ref-palette-primary30: #004e60;
+  --md-ref-palette-primary35: #005a70;
+  --md-ref-palette-primary40: #00677f;
+  --md-ref-palette-primary50: #00829f;
+  --md-ref-palette-primary60: #009ec1;
+  --md-ref-palette-primary70: #37b9de;
+  --md-ref-palette-primary80: #5cd5fb;
+  --md-ref-palette-primary90: #b6eaff;
+  --md-ref-palette-primary95: #ddf5ff;
+  --md-ref-palette-primary98: #f2fbff;
+  --md-ref-palette-primary99: #f9fdff;
+  --md-ref-palette-primary100: #ffffff;
+  /* secondary */
+  --md-ref-palette-secondary0: #000000;
+  --md-ref-palette-secondary10: #071e26;
+  --md-ref-palette-secondary20: #1e333b;
+  --md-ref-palette-secondary25: #293e47;
+  --md-ref-palette-secondary30: #344a52;
+  --md-ref-palette-secondary35: #40565e;
+  --md-ref-palette-secondary40: #4c626a;
+  --md-ref-palette-secondary50: #647a84;
+  --md-ref-palette-secondary60: #7e949e;
+  --md-ref-palette-secondary70: #98afb9;
+  --md-ref-palette-secondary80: #b3cad4;
+  --md-ref-palette-secondary90: #cfe6f1;
+  --md-ref-palette-secondary95: #ddf5ff;
+  --md-ref-palette-secondary98: #f2fbff;
+  --md-ref-palette-secondary99: #f9fdff;
+  --md-ref-palette-secondary100: #ffffff;
+  /* tertiary */
+  --md-ref-palette-tertiary0: #000000;
+  --md-ref-palette-tertiary10: #171937;
+  --md-ref-palette-tertiary20: #2c2e4d;
+  --md-ref-palette-tertiary25: #373959;
+  --md-ref-palette-tertiary30: #424465;
+  --md-ref-palette-tertiary35: #4e5071;
+  --md-ref-palette-tertiary40: #5a5c7e;
+  --md-ref-palette-tertiary50: #737498;
+  --md-ref-palette-tertiary60: #8d8eb3;
+  --md-ref-palette-tertiary70: #a7a8ce;
+  --md-ref-palette-tertiary80: #c3c3eb;
+  --md-ref-palette-tertiary90: #e0e0ff;
+  --md-ref-palette-tertiary95: #f1efff;
+  --md-ref-palette-tertiary98: #fbf8ff;
+  --md-ref-palette-tertiary99: #fffbff;
+  --md-ref-palette-tertiary100: #ffffff;
+  /* neutral */
+  --md-ref-palette-neutral0: #000000;
+  --md-ref-palette-neutral10: #191c1d;
+  --md-ref-palette-neutral20: #2e3132;
+  --md-ref-palette-neutral25: #393c3d;
+  --md-ref-palette-neutral30: #444749;
+  --md-ref-palette-neutral35: #505354;
+  --md-ref-palette-neutral40: #5c5f60;
+  --md-ref-palette-neutral50: #757779;
+  --md-ref-palette-neutral60: #8f9193;
+  --md-ref-palette-neutral70: #a9abad;
+  --md-ref-palette-neutral80: #c5c7c8;
+  --md-ref-palette-neutral90: #e1e3e4;
+  --md-ref-palette-neutral95: #eff1f2;
+  --md-ref-palette-neutral98: #f8f9fb;
+  --md-ref-palette-neutral99: #fbfcfe;
+  --md-ref-palette-neutral100: #ffffff;
+  /* neutral-variant */
+  --md-ref-palette-neutral-variant0: #000000;
+  --md-ref-palette-neutral-variant10: #151d20;
+  --md-ref-palette-neutral-variant20: #2a3235;
+  --md-ref-palette-neutral-variant25: #343d40;
+  --md-ref-palette-neutral-variant30: #40484c;
+  --md-ref-palette-neutral-variant35: #4b5457;
+  --md-ref-palette-neutral-variant40: #576063;
+  --md-ref-palette-neutral-variant50: #70787c;
+  --md-ref-palette-neutral-variant60: #8a9296;
+  --md-ref-palette-neutral-variant70: #a4acb1;
+  --md-ref-palette-neutral-variant80: #bfc8cc;
+  --md-ref-palette-neutral-variant90: #dbe4e8;
+  --md-ref-palette-neutral-variant95: #eaf2f6;
+  --md-ref-palette-neutral-variant98: #f2fbff;
+  --md-ref-palette-neutral-variant99: #f9fdff;
+  --md-ref-palette-neutral-variant100: #ffffff;
+  /* error */
+  --md-ref-palette-error0: #000000;
+  --md-ref-palette-error10: #410002;
+  --md-ref-palette-error20: #690005;
+  --md-ref-palette-error25: #7e0007;
+  --md-ref-palette-error30: #93000a;
+  --md-ref-palette-error35: #a80710;
+  --md-ref-palette-error40: #ba1a1a;
+  --md-ref-palette-error50: #de3730;
+  --md-ref-palette-error60: #ff5449;
+  --md-ref-palette-error70: #ff897d;
+  --md-ref-palette-error80: #ffb4ab;
+  --md-ref-palette-error90: #ffdad6;
+  --md-ref-palette-error95: #ffedea;
+  --md-ref-palette-error98: #fff8f7;
+  --md-ref-palette-error99: #fffbff;
+  --md-ref-palette-error100: #ffffff;
+  /* light */
+  --md-sys-color-primary-light: #00677f;
+  --md-sys-color-on-primary-light: #ffffff;
+  --md-sys-color-primary-container-light: #b6eaff;
+  --md-sys-color-on-primary-container-light: #001f28;
+  --md-sys-color-secondary-light: #4c626a;
+  --md-sys-color-on-secondary-light: #ffffff;
+  --md-sys-color-secondary-container-light: #cfe6f1;
+  --md-sys-color-on-secondary-container-light: #071e26;
+  --md-sys-color-tertiary-light: #5a5c7e;
+  --md-sys-color-on-tertiary-light: #ffffff;
+  --md-sys-color-tertiary-container-light: #e0e0ff;
+  --md-sys-color-on-tertiary-container-light: #171937;
+  --md-sys-color-error-light: #ba1a1a;
+  --md-sys-color-error-container-light: #ffdad6;
+  --md-sys-color-on-error-light: #ffffff;
+  --md-sys-color-on-error-container-light: #410002;
+  --md-sys-color-background-light: #fbfcfe;
+  --md-sys-color-on-background-light: #191c1d;
+  --md-sys-color-surface-light: #fbfcfe;
+  --md-sys-color-on-surface-light: #191c1d;
+  --md-sys-color-surface-variant-light: #dbe4e8;
+  --md-sys-color-on-surface-variant-light: #40484c;
+  --md-sys-color-outline-light: #70787c;
+  --md-sys-color-inverse-on-surface-light: #eff1f2;
+  --md-sys-color-inverse-surface-light: #2e3132;
+  --md-sys-color-inverse-primary-light: #5cd5fb;
+  --md-sys-color-shadow-light: #000000;
+  --md-sys-color-surface-tint-light: #00677f;
+  --md-sys-color-outline-variant-light: #bfc8cc;
+  --md-sys-color-scrim-light: #000000;
+  /* dark */
+  --md-sys-color-primary-dark: #5cd5fb;
+  --md-sys-color-on-primary-dark: #003543;
+  --md-sys-color-primary-container-dark: #004e60;
+  --md-sys-color-on-primary-container-dark: #b6eaff;
+  --md-sys-color-secondary-dark: #b3cad4;
+  --md-sys-color-on-secondary-dark: #1e333b;
+  --md-sys-color-secondary-container-dark: #344a52;
+  --md-sys-color-on-secondary-container-dark: #cfe6f1;
+  --md-sys-color-tertiary-dark: #c3c3eb;
+  --md-sys-color-on-tertiary-dark: #2c2e4d;
+  --md-sys-color-tertiary-container-dark: #424465;
+  --md-sys-color-on-tertiary-container-dark: #e0e0ff;
+  --md-sys-color-error-dark: #ffb4ab;
+  --md-sys-color-error-container-dark: #93000a;
+  --md-sys-color-on-error-dark: #690005;
+  --md-sys-color-on-error-container-dark: #ffdad6;
+  --md-sys-color-background-dark: #191c1d;
+  --md-sys-color-on-background-dark: #e1e3e4;
+  --md-sys-color-surface-dark: #191c1d;
+  --md-sys-color-on-surface-dark: #e1e3e4;
+  --md-sys-color-surface-variant-dark: #40484c;
+  --md-sys-color-on-surface-variant-dark: #bfc8cc;
+  --md-sys-color-outline-dark: #8a9296;
+  --md-sys-color-inverse-on-surface-dark: #191c1d;
+  --md-sys-color-inverse-surface-dark: #e1e3e4;
+  --md-sys-color-inverse-primary-dark: #00677f;
+  --md-sys-color-shadow-dark: #000000;
+  --md-sys-color-surface-tint-dark: #5cd5fb;
+  --md-sys-color-outline-variant-dark: #40484c;
+  --md-sys-color-scrim-dark: #000000;
+  /* display - large */
+  --md-sys-typescale-display-large-font-family-name: Roboto;
+  --md-sys-typescale-display-large-font-family-style: Regular;
+  --md-sys-typescale-display-large-font-weight: 400px;
+  --md-sys-typescale-display-large-font-size: 57px;
+  --md-sys-typescale-display-large-line-height: 64px;
+  --md-sys-typescale-display-large-letter-spacing: -0.25px;
+  /* display - medium */
+  --md-sys-typescale-display-medium-font-family-name: Roboto;
+  --md-sys-typescale-display-medium-font-family-style: Regular;
+  --md-sys-typescale-display-medium-font-weight: 400px;
+  --md-sys-typescale-display-medium-font-size: 45px;
+  --md-sys-typescale-display-medium-line-height: 52px;
+  --md-sys-typescale-display-medium-letter-spacing: 0px;
+  /* display - small */
+  --md-sys-typescale-display-small-font-family-name: Roboto;
+  --md-sys-typescale-display-small-font-family-style: Regular;
+  --md-sys-typescale-display-small-font-weight: 400px;
+  --md-sys-typescale-display-small-font-size: 36px;
+  --md-sys-typescale-display-small-line-height: 44px;
+  --md-sys-typescale-display-small-letter-spacing: 0px;
+  /* headline - large */
+  --md-sys-typescale-headline-large-font-family-name: Roboto;
+  --md-sys-typescale-headline-large-font-family-style: Regular;
+  --md-sys-typescale-headline-large-font-weight: 400px;
+  --md-sys-typescale-headline-large-font-size: 32px;
+  --md-sys-typescale-headline-large-line-height: 40px;
+  --md-sys-typescale-headline-large-letter-spacing: 0px;
+  /* headline - medium */
+  --md-sys-typescale-headline-medium-font-family-name: Roboto;
+  --md-sys-typescale-headline-medium-font-family-style: Regular;
+  --md-sys-typescale-headline-medium-font-weight: 400px;
+  --md-sys-typescale-headline-medium-font-size: 28px;
+  --md-sys-typescale-headline-medium-line-height: 36px;
+  --md-sys-typescale-headline-medium-letter-spacing: 0px;
+  /* headline - small */
+  --md-sys-typescale-headline-small-font-family-name: Roboto;
+  --md-sys-typescale-headline-small-font-family-style: Regular;
+  --md-sys-typescale-headline-small-font-weight: 400px;
+  --md-sys-typescale-headline-small-font-size: 24px;
+  --md-sys-typescale-headline-small-line-height: 32px;
+  --md-sys-typescale-headline-small-letter-spacing: 0px;
+  /* body - large */
+  --md-sys-typescale-body-large-font-family-name: Roboto;
+  --md-sys-typescale-body-large-font-family-style: Regular;
+  --md-sys-typescale-body-large-font-weight: 400px;
+  --md-sys-typescale-body-large-font-size: 16px;
+  --md-sys-typescale-body-large-line-height: 24px;
+  --md-sys-typescale-body-large-letter-spacing: 0.50px;
+  /* body - medium */
+  --md-sys-typescale-body-medium-font-family-name: Roboto;
+  --md-sys-typescale-body-medium-font-family-style: Regular;
+  --md-sys-typescale-body-medium-font-weight: 400px;
+  --md-sys-typescale-body-medium-font-size: 14px;
+  --md-sys-typescale-body-medium-line-height: 20px;
+  --md-sys-typescale-body-medium-letter-spacing: 0.25px;
+  /* body - small */
+  --md-sys-typescale-body-small-font-family-name: Roboto;
+  --md-sys-typescale-body-small-font-family-style: Regular;
+  --md-sys-typescale-body-small-font-weight: 400px;
+  --md-sys-typescale-body-small-font-size: 12px;
+  --md-sys-typescale-body-small-line-height: 16px;
+  --md-sys-typescale-body-small-letter-spacing: 0.40px;
+  /* label - large */
+  --md-sys-typescale-label-large-font-family-name: Roboto;
+  --md-sys-typescale-label-large-font-family-style: Medium;
+  --md-sys-typescale-label-large-font-weight: 500px;
+  --md-sys-typescale-label-large-font-size: 14px;
+  --md-sys-typescale-label-large-line-height: 20px;
+  --md-sys-typescale-label-large-letter-spacing: 0.10px;
+  /* label - medium */
+  --md-sys-typescale-label-medium-font-family-name: Roboto;
+  --md-sys-typescale-label-medium-font-family-style: Medium;
+  --md-sys-typescale-label-medium-font-weight: 500px;
+  --md-sys-typescale-label-medium-font-size: 12px;
+  --md-sys-typescale-label-medium-line-height: 16px;
+  --md-sys-typescale-label-medium-letter-spacing: 0.50px;
+  /* label - small */
+  --md-sys-typescale-label-small-font-family-name: Roboto;
+  --md-sys-typescale-label-small-font-family-style: Medium;
+  --md-sys-typescale-label-small-font-weight: 500px;
+  --md-sys-typescale-label-small-font-size: 11px;
+  --md-sys-typescale-label-small-line-height: 16px;
+  --md-sys-typescale-label-small-letter-spacing: 0.50px;
+  /* title - large */
+  --md-sys-typescale-title-large-font-family-name: Roboto;
+  --md-sys-typescale-title-large-font-family-style: Regular;
+  --md-sys-typescale-title-large-font-weight: 400px;
+  --md-sys-typescale-title-large-font-size: 22px;
+  --md-sys-typescale-title-large-line-height: 28px;
+  --md-sys-typescale-title-large-letter-spacing: 0px;
+  /* title - medium */
+  --md-sys-typescale-title-medium-font-family-name: Roboto;
+  --md-sys-typescale-title-medium-font-family-style: Medium;
+  --md-sys-typescale-title-medium-font-weight: 500px;
+  --md-sys-typescale-title-medium-font-size: 16px;
+  --md-sys-typescale-title-medium-line-height: 24px;
+  --md-sys-typescale-title-medium-letter-spacing: 0.15px;
+  /* title - small */
+  --md-sys-typescale-title-small-font-family-name: Roboto;
+  --md-sys-typescale-title-small-font-family-style: Medium;
+  --md-sys-typescale-title-small-font-weight: 500px;
+  --md-sys-typescale-title-small-font-size: 14px;
+  --md-sys-typescale-title-small-line-height: 20px;
+  --md-sys-typescale-title-small-letter-spacing: 0.10px;
+}
+`
+
+export const Theme = {
+  Primary: '--md-sys-color-primary',
+  OnPrimary: '--md-sys-color-on-primary',
+  PrimaryContainer: '--md-sys-color-primary-container',
+  OnPrimaryContainer: '--md-sys-color-on-primary-container',
+
+  Secondary: '--md-sys-color-secondary',
+  OnSecondary: '--md-sys-color-on-secondary',
+  SecondaryContainer: '--md-sys-color-secondary-container',
+  OnSecondaryContainer: '--md-sys-color-on-secondary-container',
+
+  Tertiary: '--md-sys-color-tertiary',
+  OnTertiary: '--md-sys-color-on-tertiary',
+  TertiaryContainer: '--md-sys-color-tertiary-container',
+  OnTertiaryContainer: '--md-sys-color-on-tertiary-container',
+
+  Error: '--md-sys-color-error',
+  OnError: '--md-sys-color-on-error',
+  ErrorContainer: '--md-sys-color-error-container',
+  OnErrorContainer: '--md-sys-color-on-error-container',
+
+  Background: '--md-sys-color-background',
+  OnBackground: '--md-sys-color-on-background',
+
+  Surface: '--md-sys-color-surface',
+  OnSurface: '--md-sys-color-on-surface',
+  SurfaceVariant: '--md-sys-color-surface-variant',
+  OnSurfaceVariant: '--md-sys-color-on-surface-variant',
+
+  Outline: '--md-sys-color-outline',
+  InverseOnSurface: '--md-sys-color-inverse-on-surface',
+  InverseSurface: '--md-sys-color-inverse-surface',
+  InversePrimary: '--md-sys-color-inverse-primary',
+
+  Shadow: '--md-sys-color-shadow',
+  SurfaceTint: '--md-sys-color-surface-tint',
+  OutlineVariant: '--md-sys-color-outline-variant',
+  Scrim: '--md-sys-color-scrim',
+}
+
+type Color = keyof typeof Theme;
+
+export enum ThemeMode {
+  Dark = 'dark',
+  Light = 'light',
+}
+
+/**
+ * Generate tokens for dark & light modes
+ */
+export const GenerateTheme = (mode: ThemeMode) => {
+  return Object.values(Theme).reduce((acc, variable) => {
+    acc[variable as Color] = `var(${variable}-${mode})`;
     return acc;
-  }, {} as Record<string, string>)
+  }, {} as Record<Color, string>);
+}
 
+/**
+ * Update tokens from hex => rgb
+ */
+const regexp = {
+  var: /(--)[^\,\:\)]+/g,
+  hex: /#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})/g,
+  value: /(?<=((--)[^\,\:\)]+: ))(.*)(?=;)/g,
+}
 
-const darkColors = tokens.entities
-  .filter(entitiy => entitiy.type === 'color')
-  .reduce((acc, token) => {
-    const name = token.name.split('.');
-    const isDark = name.at(-1) === 'dark';
-    if (isDark) {
-      /**
-       * colorName - name of color without prefixes
-       * @Example md.sys.color.secondary-container.light => secondaryContainer
-       */
-      let colorName = name.slice(3, -1).join('');
-      acc[toCamelCase(colorName)] = token.value;
+/**
+ * Parse tokens from CSS file to Map<MD3Token, MD3Color>
+ * @param tokens CSS file with tokens from https://m3.material.io/theme-builder#/custom
+ */
+export const GenerateCSS = (tokens = cssTokens) => {
+  const styles: Record<string, string> = {};
+  const lines = tokens.split('\n');
+
+  for (const line of lines) {
+    try {
+      const property = parseLine(line);
+      styles[property.variable] = property.value;
+    } catch (error) {
+      console.error('error:', error);
     }
-    return acc;
-  }, {} as Record<string, string>);
+  }
 
+  return styles;
+}
 
-export const tokenColorsLight = lightColors;
-export const tokenColorsDark = darkColors;
+function parseLine(line: string): { variable: string, value: string } {
+  const [variable] = line.match(regexp.var) || [];
+  if (variable) {
+    const [hex] = line.match(regexp.hex) || [];
+    if (hex) {
+      const rgb = hexToRGB(hex);
+      if (rgb) {
+        return { variable, value: rgb };
+      } else {
+        throw new Error(`cannot parse hex: ${hex}`);
+      }
+    } else {
+      const [param] = line.match(regexp.value) || [];
+      if (param) {
+        return { variable, value: param };
+      } else {
+        throw new Error(`cannot parse line: ${line}`);
+      }
+    }
+  }
+}
+
+function hexToRGB(hex: string) {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  if (result) {
+    const rgb = {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    }
+
+    return `${rgb.r} ${rgb.g} ${rgb.b}`;
+  }
+
+  return null;
+}
