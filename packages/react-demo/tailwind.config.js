a644bf901bb027fd8a55b9f5f7ad697afec26ef5
Alexander <valverin95@gmail.com>
2023-10-09 16:54:18 +0300

    React: add custom tokens

--- packages/react-demo/tailwind.config.js
+++ packages/react-demo/tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+const fs = require("fs");
+const path = require("path");
 const { md3Theme } = require("../theme/dist");
 const { md3Components } = require("../components/dist");
+
+const tokens = fs.readFileSync(
+  path.resolve(__dirname, "src", "css", "tokens.css"),
+  "utf-8"
+);
 
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {},
   },
-  plugins: [md3Theme(), md3Components],
+  plugins: [md3Theme({ tokens }), md3Components],
 };
