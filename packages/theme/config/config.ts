0c425fffc45acb79f0380a9f658771cfbe622942
Alexander <valverin95@gmail.com>
2023-10-12 14:56:21 +0300

    add config

---
+++ packages/theme/config/config.ts
@@ -0,0 +1,13 @@
+export type md3ThemeConfig = {
+  /**
+   * CSS tokens from Material Design 3
+   */
+  tokens?: string;
+  colors?: Record<string, string>;
+  stateLayers?: {
+    hover?: number;
+    press?: number;
+    focus?: number;
+    drag?: number;
+  };
+};
