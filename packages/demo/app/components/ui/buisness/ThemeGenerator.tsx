976bd378248eab579893148f0db08ffbbae8be95
Alexander <valverin95@gmail.com>
2023-10-15 21:30:23 +0300

    feat: [Demo] save tokens

--- packages/demo/app/components/ui/buisness/ThemeGenerator.tsx
+++ packages/demo/app/components/ui/buisness/ThemeGenerator.tsx
@@ -1,37 +1,50 @@
 "use client";
 import { THEME_SOURCE } from "@/app/constants/theme";
-import { TextField } from "../form/TextField";
 import { useEffect, useRef, useState } from "react";
 import { genTokens } from "@/app/theme/tokens";
+
+const THEME_TOKENS_SOURCE = "theme-tokens";
+type Tokens = ReturnType<typeof genTokens>;
 
 export function ThemeGenerator() {
   const colorPicker = useRef<HTMLInputElement>(null);
   const [state, setState] = useState({
-    source: THEME_SOURCE,
+    source: '',
   });
 
   useEffect(() => {
-    generateTheme();
+    const source = localStorage.getItem(THEME_TOKENS_SOURCE) || THEME_SOURCE;
+    setState((state) => ({ ...state, source }));
+  }, []);
+
+  useEffect(() => {
+    if (state.source) {
+      generateTheme();
+    }
   }, [state.source]);
 
   const generateTheme = () => {
     const root = document.querySelector(":root");
     if (root instanceof HTMLElement) {
       const tokens = genTokens(state.source);
-
-      Object.entries(tokens.palettes).forEach(([variable, value]) => {
-        root.style.setProperty(variable, value);
-      });
-
-      Object.entries(tokens.dark).forEach(([variable, value]) => {
-        root.style.setProperty(variable, value);
-      });
-
-      Object.entries(tokens.light).forEach(([variable, value]) => {
-        root.style.setProperty(variable, value);
-      });
+      localStorage.setItem(THEME_TOKENS_SOURCE, state.source);
+      applyTheme(tokens, root);
     }
   };
+
+  function applyTheme(tokens: Tokens, el: HTMLElement) {
+    Object.entries(tokens.palettes).forEach(([variable, value]) => {
+      el.style.setProperty(variable, value);
+    });
+
+    Object.entries(tokens.dark).forEach(([variable, value]) => {
+      el.style.setProperty(variable, value);
+    });
+
+    Object.entries(tokens.light).forEach(([variable, value]) => {
+      el.style.setProperty(variable, value);
+    });
+  }
 
   return (
     <div>
