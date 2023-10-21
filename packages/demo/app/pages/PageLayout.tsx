a24cc4a91e365ea60eff67766206f0abb4ae7126
Alexander <valverin95@gmail.com>
2023-10-21 22:22:18 +0300

    refactor: [Demo] template without anchorMenu

--- packages/demo/app/pages/PageLayout.tsx
+++ packages/demo/app/pages/PageLayout.tsx
@@ -1,16 +1,18 @@
 import { ReactNode } from "react";
 
 export type PageLayoutProps = {
-  anchorMenu: ReactNode;
+  anchorMenu?: ReactNode;
   children: ReactNode;
 };
 
 export function PageLayout({ anchorMenu, children }: PageLayoutProps) {
   return (
     <div className="flex flex-col md:flex-row items-start gap-2 h-full">
-      <aside className="py-4 p-2 block md:sticky top-[10px]">
-        {anchorMenu}
-      </aside>
+      {anchorMenu && (
+        <aside className="py-4 p-2 block md:sticky top-[10px]">
+          {anchorMenu}
+        </aside>
+      )}
 
       <main className="flex-1 w-full flex flex-col items-start gap-2 px-4 overflow-hidden rounded-[24px]">
         {children}
--- packages/demo/app/pages/elevation/page.tsx
+++ packages/demo/app/pages/elevation/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
 import { PageLayout } from "../PageLayout";
-import { AnchorMenu } from "../AnchorMenu";
+import { Surface } from "@/app/components/ui/surface/Surface";
 
 export const metadata: Metadata = {
   title: "MD3 ui | Elevation",
@@ -9,16 +9,30 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <PageLayout
-      anchorMenu={
-        <AnchorMenu>
-          <AnchorMenu.Item href="" label="Light Scheme" />
-          <AnchorMenu.Item href="" label="Dark Scheme" />
-          <AnchorMenu.Item href="" label="Elevations" />
-        </AnchorMenu>
-      }
-    >
+    <PageLayout>
       <p className="title-large mb-4">Elevation demo</p>
+
+      <div className="flex flex-col w-full gap-6 py-6">
+        <Surface>
+          <div className="flex p-2 justify-evenly gap-4">
+            <div className="h-[180px] flex items-center justify-center rounded w-full elevation-1">
+              <span className="label-large">1</span>
+            </div>
+            <div className="h-[180px] flex items-center justify-center rounded w-full elevation-2">
+              <span className="label-large">2</span>
+            </div>
+            <div className="h-[180px] flex items-center justify-center rounded w-full elevation-3">
+              <span className="label-large">3</span>
+            </div>
+            <div className="h-[180px] flex items-center justify-center rounded w-full elevation-4">
+              <span className="label-large">4</span>
+            </div>
+            <div className="h-[180px] flex items-center justify-center rounded w-full elevation-5">
+              <span className="label-large">5</span>
+            </div>
+          </div>
+        </Surface>
+      </div>
     </PageLayout>
   );
 }
