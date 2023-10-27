3b1122858dc031e89a489f2cc87ed91246426519
Alexander <valverin95@gmail.com>
2023-10-28 00:49:03 +0300

    feat: [React] Icon

---
+++ packages/react/src/Icon/Icon.ts
@@ -0,0 +1,10 @@
+'use client';
+import React from "react";
+import { createComponent } from "@lit-labs/react";
+import { MdIcon } from "@material/web/icon/icon";
+
+export const Icon = createComponent({
+  react: React,
+  tagName: "md-icon",
+  elementClass: MdIcon,
+});
