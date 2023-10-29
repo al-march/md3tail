433c6424a7d6957e7e55759403e869584a8192e0
Alexander <valverin95@gmail.com>
2023-10-29 04:14:30 +0300

    feat: [Demo] metadata for components page

--- packages/demo/app/pages/components/page.tsx
+++ packages/demo/app/pages/components/page.tsx
@@ -3,6 +3,12 @@ import { Title } from "./shared/typography";
 import { Actions } from "./actions";
 import { Inputs } from "./inputs";
 import { Navigations } from "./navigations";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "md3tail | Components",
+  description: "Material Design 3 components examples",
+};
 
 export default function Page() {
   return (
