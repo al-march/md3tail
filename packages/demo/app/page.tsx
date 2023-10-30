dbcb5194fd5e9b8e69f1c4abc2ab0778a877633b
Alexander <valverin95@gmail.com>
2023-10-30 03:20:22 +0300

    feat: [Demo] update main page

--- packages/demo/app/page.tsx
+++ packages/demo/app/page.tsx
@@ -1,11 +1,11 @@
+import { Divider, Icon, TextButton } from "@md3tail/react";
 import { Surface } from "./components/ui/surface/Surface";
+import { Title } from "./pages/components/shared/typography";
+import Link from "next/link";
 
-const installNpm = `npm i @md3tail/theme`;
-const installYarn = `yarn add @md3tail/theme`;
-const installPnpm = `pnpm i @md3tail/theme`;
-const installBun = `bun i @md3tail/theme`;
+const installTheme = `npm i @md3tail/theme`;
 
-const usageCode = `
+const usageThemeCode = `
 import type { Config } from 'tailwindcss';
 import { md3Theme } from '@md3tail/theme';
 
@@ -16,32 +16,39 @@ const config: Config = {
 export default config;
 `.trim();
 
+const installReact = `npm i @md3tail/react`;
+
+const usageReact = `
+import {
+  TextButton,
+  Radio,
+  Row,
+} from "@md3tail/react";
+
+export default function Page() {
+  return (
+    <Row className="w-full" items="center">
+      <TextButton>
+        <Icon slot="icon">favorite</Icon>
+        <span>Icon</span>
+      </TextButton>
+    </Row>
+  );
+}
+`.trim();
+
 export default function Home() {
   return (
     <div className="px-2 py-4">
-      <h1 className="title-medium mb-4">Install</h1>
+      <Title>Tailwind plugin</Title>
+      <br />
+      <Divider></Divider>
+      <br />
+      <h2 className="title-medium mb-4">Install</h2>
 
       <Surface className="my-2">
         <code>
-          <pre>{installNpm}</pre>
-        </code>
-      </Surface>
-
-      <Surface className="my-2">
-        <code>
-          <pre>{installPnpm}</pre>
-        </code>
-      </Surface>
-
-      <Surface className="my-2">
-        <code>
-          <pre>{installYarn}</pre>
-        </code>
-      </Surface>
-
-      <Surface className="my-2">
-        <code>
-          <pre>{installBun}</pre>
+          <pre>{installTheme}</pre>
         </code>
       </Surface>
 
@@ -50,9 +57,48 @@ export default function Home() {
 
       <Surface className="my-2">
         <code>
-          <pre>{usageCode}</pre>
+          <pre className="overflow-y-auto">{usageThemeCode}</pre>
         </code>
       </Surface>
+
+      <br />
+
+      <Title>React components</Title>
+      <br />
+      <Divider></Divider>
+      <br />
+      <h2 className="title-medium mb-4">Install</h2>
+
+      <Surface className="my-2">
+        <code>
+          <pre>{installReact}</pre>
+        </code>
+      </Surface>
+
+      <h2 className="title-medium mb-4">Usage</h2>
+
+      <p>
+        <Link
+          className="text-primary label-medium"
+          href="https://www.npmjs.com/package/@md3tail/react"
+          target="_blank"
+        >
+          @md3tail/react
+        </Link>{" "}
+        is a wrapper around Material Desingn 3 components
+      </p>
+
+      <Surface className="my-2">
+        <code>
+          <pre className="overflow-y-auto">{usageReact}</pre>
+        </code>
+      </Surface>
+
+      <Link href="pages/components">
+        <TextButton>
+          <Icon slot="icon">gallery_thumbnail</Icon> Preview
+        </TextButton>
+      </Link>
     </div>
   );
 }
